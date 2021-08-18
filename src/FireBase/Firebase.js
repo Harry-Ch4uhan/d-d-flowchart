e4156c816f35bbfd4ecce3181df89c497a4df26d
Harry-Ch4uhan <85424351+Harry-Ch4uhan@users.noreply.github.com>
2021-08-18 18:04:28 +0530

    removed firebase details

--- src/FireBase/Firebase.js
+++ src/FireBase/Firebase.js
@@ -1,16 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAFddnquKFe8_gvp3UqnXmi-a-9hK55w3s",
-  authDomain: "hooks-practice-c1a8c.firebaseapp.com",
-  databaseURL: "https://hooks-practice-c1a8c-default-rtdb.firebaseio.com",
-  projectId: "hooks-practice-c1a8c",
-  storageBucket: "hooks-practice-c1a8c.appspot.com",
-  messagingSenderId: "594767013987",
-  appId: "1:594767013987:web:e249c7e703fb8b747af056",
-  measurementId: "G-RWWWNWGWFY",
-};
+const firebaseConfig = {...};
 
 firebase.initializeApp(firebaseConfig);
 
